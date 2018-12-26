c69d92d36177f800b4396305e9573a651c9e5304
donghao <1341479360@qq.com>
2018-12-26 11:45:18 +0800

    增加分支1

--- src/java/Demo.java
+++ src/java/Demo.java
@@ -12,12 +12,6 @@ public class Demo {
 
 
         System.out.println("开发分支");
-        System.out.println("新的分支");
-        System.out.println("添加新的功能");
-
-        System.out.println("分支1");
-
-        System.out.println("又添加了功能2");
-        System.out.println("功能1删除");
+        System.out.println("添加功能1");
     }
 }
